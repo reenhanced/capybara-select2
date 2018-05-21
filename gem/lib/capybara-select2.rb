bef027bd3d56fc2d2e5522f4fbd36673d5592937
Luciano <marcelinolucianom@gmail.com>
2018-05-21 14:42:07 -0300

    Get the open select2 input

--- gem/lib/capybara-select2.rb
+++ gem/lib/capybara-select2.rb
@@ -27,7 +27,7 @@ module Capybara
       end
 
       if options.has_key? :search
-        find(:xpath, "//body").find(".select2-search input.select2-search__field").set(value)
+        find(:xpath, "//body").find(".select2-container--open .select2-search input.select2-search__field").set(value)
         page.execute_script(%|$("input.select2-search__field:visible").keyup();|)
         @drop_container = ".select2-results"
       elsif find(:xpath, "//body").has_selector?(".select2-dropdown")
